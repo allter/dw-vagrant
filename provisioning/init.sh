5900ddb7d5f9c650631014bd1ed028fc7a278c68
Allter <allter@hague>
2017-05-04 23:44:43 +0300

    init.sh kinda works

--- provisioning/init.sh
+++ provisioning/init.sh
@@ -2,12 +2,17 @@
 
 echo "Updating apt repository"
 apt-get -qq update
+
 echo "Installing prerequisite packages"
-apt-get -qq install git python-dev python-pip
-apt-get -qq install libssl-dev libffi-dev python-dev
+apt-get -qq install git python-dev python-pip \
+	libssl-dev libffi-dev
+
+echo "Installing ansible"
 pip -q install markupsafe
 pip -q install Jinja2==2.8.1
 pip -q install git+git://github.com/ansible/ansible.git@v2.0.0-0.5.beta3
+
+#apt-get -qq install ansible
 
 cd /vagrant/provisioning
 ansible-playbook build_dev_vm.yml
