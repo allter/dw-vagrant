16c35b6940ab270801ac693051cf223e67760e48
Allter <allter@hague>
2017-05-06 01:06:41 +0300

    Install ansible using system apt

--- provisioning/init.sh
+++ provisioning/init.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Updating apt repository"
-apt-get -qq update
+#echo "Updating apt repository"
+#apt-get -qq update
 
-echo "Installing prerequisite packages"
-apt-get -qq install git python-dev python-pip \
-	libssl-dev libffi-dev
+#echo "Installing prerequisite packages"
+#apt-get -qq install git python-dev python-pip \
+#	libssl-dev libffi-dev
+#
+#echo "Installing ansible"
+#pip -q install markupsafe
+#pip -q install Jinja2==2.8.1
+#pip -q install git+git://github.com/ansible/ansible.git@v2.0.0-0.5.beta3
 
 echo "Installing ansible"
-pip -q install markupsafe
-pip -q install Jinja2==2.8.1
-pip -q install git+git://github.com/ansible/ansible.git@v2.0.0-0.5.beta3
+apt-get -qq install ansible
 
-#apt-get -qq install ansible
-
+echo "Provisioning using ansible"
 cd /vagrant/provisioning
 ansible-playbook build_dev_vm.yml
